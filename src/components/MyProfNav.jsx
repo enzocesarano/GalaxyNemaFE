025115808ef851c35ba0a9701b6d966ed77a25f0
Vincenzo Cesarano <vincenzo.cesarano92@gmail.com>
2024-11-21 10:04:07 +0100

    up

--- src/components/MyProfNav.jsx
+++ src/components/MyProfNav.jsx
@@ -6,11 +6,12 @@ const MyProfNav = () =>{
 
     return(
         <div className="d-flex fs-5 align-items-center">
-            <i className="bi bi-bell-fill text-secondary me-3"></i>
-            <p className="text-secondary p-0 m-0 me-3">Vincenzo</p>
-            <div className="w-10">
+            <div className="w-10 me-3">
                 <Image src="https://starwalk.space/gallery/images/full-moon-black-tree/1140x641.jpg" alt="" className="imageProfile w-100 h-100 object-fit-cover rounded-circle" />
             </div>
+        
+            <p className="text-secondary p-0 m-0 me-3">Vincenzo</p>
+            <i className="bi bi-bell-fill text-secondary "></i>
         </div>
     )
 }
