ffbcb03e7c4548cee6b3d37e54d37ca258997fe9
Vincenzo Cesarano <vincenzo.cesarano92@gmail.com>
2024-12-03 13:42:15 +0100

    up

--- src/components/MyFilmSingle.jsx
+++ src/components/MyFilmSingle.jsx
@@ -6,7 +6,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import {
   addPreferiti,
-  getPreferiti,
   removePreferiti,
   selectProiezioneAction,
 } from "../redux/actions";
